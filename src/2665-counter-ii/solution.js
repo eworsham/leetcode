d23ab2e9505ccffb5e17fc7d6d190d68886d2cbb
eworsham <eworsham64@gmail.com>
2025-06-06 21:24:04 -0400

    Started on problem 2665

---
+++ src/2665-counter-ii/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    return {
+        increment() {
+
+        },
+        decrement() {
+
+        },
+        reset() {
+            
+        }
+    }
+};
+
+module.exports = createCounter;
---
+++ src/2665-counter-ii/solution.test.js
@@ -0,0 +1,9 @@
+const createCounter = require('./solution');
+
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
