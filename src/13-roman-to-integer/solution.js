7f886fb1fbc8e146f42c9559b75e35b006952398
Ryan Worsham <eworsham64@gmail.com>
2025-06-27 22:58:43 -0400

    Refactor romanToInt function to simplify logic and improve readability

--- src/13-roman-to-integer/solution.js
+++ src/13-roman-to-integer/solution.js
@@ -14,32 +14,19 @@ var romanToInt = function(s) {
         ['M', 1000]
     ]);
 
-    // Loop through string calculating value of roman numeral
-    let romanNumeralIntValue = 0;
-    let currentIndex = 0;
-    while (currentIndex < s.length) {
-        // Grab current and next roman numeral
-        const currentRomanNumeral = s.charAt(currentIndex);
-        const nextRomanNumeral = s.charAt(currentIndex + 1);
-        
-        // 'I' can be placed before V and X to make 4 and 9
-        // 'X' can be placed before L and C to make 40 and 90
-        // 'C' can be placed before D and M to make 400 and 900
-        if (
-            ( currentRomanNumeral === 'I' && ( nextRomanNumeral === 'V' || nextRomanNumeral === 'X' ) ) ||
-            ( currentRomanNumeral === 'X' && ( nextRomanNumeral === 'L' || nextRomanNumeral === 'C' ) ) ||
-            ( currentRomanNumeral === 'C' && ( nextRomanNumeral === 'D' || nextRomanNumeral === 'M' ) )
-        ) {
-            romanNumeralIntValue += romanNumeralIntValues.get(nextRomanNumeral) - romanNumeralIntValues.get(currentRomanNumeral);
-            currentIndex += 2;
+    // Loop through each character in the string, adding or subtracting values based on Roman numeral rules
+    let romanNumberIntValue = 0;
+    for (let i = 0; i < s.length; i++) {
+        const currentValue = romanNumeralIntValues.get(s[i]);
+        const nextValue = romanNumeralIntValues.get(s[i + 1]);
+        if (nextValue && currentValue < nextValue) {
+            romanNumberIntValue -= currentValue;
         } else {
-            romanNumeralIntValue += romanNumeralIntValues.get(currentRomanNumeral)
-            currentIndex++;
+            romanNumberIntValue += currentValue;
         }
     }
 
-    // Return value of roman numeral
-    return romanNumeralIntValue;
+    return romanNumberIntValue;
 };
 
 module.exports = romanToInt;
