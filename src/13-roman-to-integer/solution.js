b39dd693964c86318e5fe19a3a9b199bae6c3901
Ryan Worsham <eworsham64@gmail.com>
2025-06-27 23:04:25 -0400

    Refactor romanToInt function to use an object instead of a Map for numeral values

--- src/13-roman-to-integer/solution.js
+++ src/13-roman-to-integer/solution.js
@@ -3,30 +3,25 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    // Map to store values of all possible roman numeral values
-    const romanNumeralIntValues = new Map([
-        ['I', 1],
-        ['V', 5],
-        ['X', 10],
-        ['L', 50],
-        ['C', 100],
-        ['D', 500],
-        ['M', 1000]
-    ]);
+    // Store values of all possible roman numeral values
+    const romanNumeralValues = {
+        I: 1, V: 5, X: 10, L: 50,
+        C: 100, D: 500, M: 1000
+    };
 
     // Loop through each character in the string, adding or subtracting values based on Roman numeral rules
-    let romanNumberIntValue = 0;
+    let totalValue = 0;
     for (let i = 0; i < s.length; i++) {
-        const currentValue = romanNumeralIntValues.get(s[i]);
-        const nextValue = romanNumeralIntValues.get(s[i + 1]);
+        const currentValue = romanNumeralValues[s[i]];
+        const nextValue = romanNumeralValues[s[i + 1]];
         if (nextValue && currentValue < nextValue) {
-            romanNumberIntValue -= currentValue;
+            totalValue -= currentValue;
         } else {
-            romanNumberIntValue += currentValue;
+            totalValue += currentValue;
         }
     }
 
-    return romanNumberIntValue;
+    return totalValue;
 };
 
 module.exports = romanToInt;
